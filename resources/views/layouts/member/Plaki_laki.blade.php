f84140459c19caf20c90b430d008330235b8f975
NovalRipa <rippa00@gmail>
2021-12-30 16:48:25 +0700

    12/30/2021

--- resources/views/layouts/member/Plaki_laki.blade.php
+++ resources/views/layouts/member/Plaki_laki.blade.php
@@ -306,3 +306,7 @@
       </div>
   </div>
 </div>
+
+<!-- partial -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
